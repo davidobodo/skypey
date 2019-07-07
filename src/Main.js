ca8676cf2bd54f5c2dc52f0251239072dee88491
David Obodo <obododavid5@gmail.com>
2019-07-07 21:17:44 +0100

    css applied but flex property for main component not working

---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Main.css'
+
+const Main = () => {
+	return (
+		<div>
+			<main className="Main">Main</main>
+		</div>
+	)
+}
+
+export default Main
---
+++ src/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Sidebar.css'
+
+const Sidebar = () => {
+	return (
+		<div>
+			<aside className="Sidebar">Sidebar</aside>
+		</div>
+	)
+}
+
+export default Sidebar
